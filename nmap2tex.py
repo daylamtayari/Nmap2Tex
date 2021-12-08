1e0c578e1e5fc0164493f3787757c3de5a9f26ec
Daylam Tayari <git@tayari.gg>
2021-12-08 15:58:49 -0700

    Created file and completed input handling

---
+++ nmap2tex.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python3
+
+__version__ = 0.1
+__author__ = 'Daylam Tayari'
+
+import sys
+
+
+# Global Variables:
+
+xmlFile = ''
+latexFile = ''
+
+
+# Handle Inputs:
+
+def invalidInput(num):
+    print('Nmap2Tex '+str(__version__))
+    if num == 0:
+        print('Invalid Input: No inputs provided.')
+    if num == 1:
+        print('Invalid Input: Only one input provided.')
+    if num == 2:
+        print('Invalid Input: Only two inputs must be provided.')
+    print('Usage: nmap2tex [Nmap XML file] [Output LaTeX file]')
+    print('The Nmap file provided must be an Nmap scan output file formatted in Nmap\'s XML format.')
+
+
+def inputHandling():
+    if len(sys.argv) == 1 or len(sys.argv) == 2:
+        if len(sys.argv) == 1:
+            return invalidInput(0)
+    if len(sys.argv) == 2:
+        if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+            return invalidInput(-1)
+        else:
+            return invalidInput(1)
+    if len(sys.argv) > 3:
+        return invalidInput(2)
+    else:
+        global xmlFile
+        xmlFile = sys.argv[1]
+        global latexFile
+        latexFile = sys.argv[2]
+
+
+inputHandling()
